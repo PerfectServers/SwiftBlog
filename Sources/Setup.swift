33cf65c32d2bec089420dcdc75f9ba54751b539f
Jonathan Guthrie <jono@perfect.org>
2017-04-22 18:08:49 -0500

    creating new user if none is there

--- Sources/Setup.swift
+++ Sources/Setup.swift
@@ -8,12 +8,14 @@
 
 import PerfectTurnstilePostgreSQL
 import PostgresStORM
+import TurnstileCrypto
+import Turnstile
 
 let site = Site()
 func setupSystem() {
 	// Set up the Authentication table
 	let auth = AuthAccount()
-	try? auth.setup()
+	auth.setup()
 
 	// Connect the AccessTokenStore
 	tokenStore = AccessTokenStore()
@@ -31,4 +33,24 @@ func setupSystem() {
 	try? site.setupTable()
 	site.getSiteInfo()
 
+}
+
+extension AuthAccount {
+	func setup() {
+		do {
+			try super.setup()
+			try findAll()
+			if rows().count == 0 {
+				let r = URandom()
+				let p = r.secureToken
+				id(r.secureToken)
+				username = "Admin"
+				password = BCrypt.hash(password: p)
+				print("No user accounts, createing default login 'Admin' with password '\(p)'")
+				try create()
+			}
+		} catch {
+			print("Error: \(error)")
+		}
+	}
 }
